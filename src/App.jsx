a3fb356fdfee20da8a3eb2325d867d19114a6254
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-05-05 09:56:50 +0600

    color modify

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,16 @@ function App() {
         className="container1 bg-white p-4 row  shadow-custom "
         style={{ borderRadius: "10px", height: "745px" }}
       >
-        <div className="col-md-5 col-12 d-flex flex-column justify-content-center">
+        <div className="col-md-5 col-12 d-flex flex-column justify-content-center p-4">
           <h4
-            className="text-lg font-weight-bold pb-2"
+            className="text-lg fw-bold pb-2"
             style={{ color: "#000c37" }}
           >
             Welcome!
           </h4>
           <p
             className="text-muted font-size-14 font-weight-normal"
-            style={{ lineHeight: "1.5rem" }}
+            style={{ lineHeight: "1.5rem" ,color:"rgba(33, 37, 41, 0.75)"}}
           >
             We are glad to see you
           </p>
@@ -64,7 +64,7 @@ function App() {
             >
               <span
                 className="text-decoration-underline text-uppercase fw-normal absolute "
-                style={{ fontSize: "10px" }}
+                style={{ fontSize: "10px",color:"#000c37" }}
               >
                 Forget Password?
               </span>
@@ -80,18 +80,22 @@ function App() {
 
             <Form.Item>
               <button htmlType="submit" className="submit w-full ">
-                Submit
+                Sign in
               </button>
             </Form.Item>
           </Form>
-          <p className="text-center">Or continue with</p>
-          <div className="d-flex justify-content-center gap-2">
-            <span className="p-11 btn flex gap-2 align-items-center">
-              <FcGoogle /> Google
-            </span>
-            <span className="p-11 btn flex gap-2 align-items-center">
-              <IoLogoFacebook style={{ color: "#08f" }} /> Facebook
-            </span>
+          <p className="text-center"style={{color:"#6e7e92"}}>Or continue with</p>
+          <div className="row row-cols-2 g-3">
+            <div>
+              <span className=" col btn1 d-flex gap-2 align-items-center justify-content-center col">
+                <FcGoogle size={24} /> Google
+              </span>
+            </div>
+            <div>
+              <span className=" col btn1 d-flex gap-2 align-items-center justify-content-center col">
+                <IoLogoFacebook style={{ color: "#08f" }} size={24} /> Facebook
+              </span>
+            </div>
           </div>
           <p
             className="text-center py-2 fw-normal"
@@ -101,7 +105,7 @@ function App() {
             <span className="text-decoration-underline">Sign up</span>
           </p>
         </div>
-        <div className="col-md-7 col-12 h-full">
+        <div className="col-md-7 col-12 h-full d-md-block d-none">
           <img
             src="/banner.png"
             className="img-fluid"
